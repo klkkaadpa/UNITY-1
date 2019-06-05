091b5326d00303dbc17b9dd69770f1590f392056
klkkaadpa@gmail.com <IOIIAADPa2>
2019-06-05 13:14:21 +0800

    狗的類型  叫的方法

--- New Unity Project/Assets/SCRIPTS/Dog.cs
+++ New Unity Project/Assets/SCRIPTS/Dog.cs
@@ -8,14 +8,30 @@ public class Dog
     public string type;
     public string sex;
     public string color;
-    public string age;
+    public int age;
     #endregion
-
 
     #region 方法
     public void Bark()
     {
         Debug.Log(name + "：汪汪!!!");
+    }
+
+    public int ConvertAge()
+    {
+        return age * 7;
+    }
+
+    public void Eat(string food, string speed = "慢")
+    {
+        Debug.Log(name + "：" + food + "，速度：" + speed);
+    }
+
+
+
+    public void Eat(string food, int count)
+    {
+        Debug.Log(name + "：" + food + "，份量：" + count);
     }
     #endregion
 
